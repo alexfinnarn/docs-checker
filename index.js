e55a6c9cc2b3d18d0288a532720f5a566ae3dd6f
Alex Finnarn <alex.finnarn@gmail.com>
2019-07-23 15:04:25 -0400

    switch to single quotes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  */
 module.exports = app => {
   app.on(
-    ["pull_request.opened", "pull_request.reopened", "pull_request.edited"],
+    ['pull_request.opened', 'pull_request.reopened', 'pull_request.edited'],
     async context => {
-      const config = await context.config("docs-checker.yml", {
+      const config = await context.config('docs-checker.yml', {
         minChangedLines: 40,
         docscheck: true
       });
@@ -20,31 +20,31 @@ module.exports = app => {
       // Get all the added lines into one array.
       // The patch property had all the info I needed at the time.
       const actualChanges = allFiles.map(e =>
-        e.patch.split("\n").filter(f => f.startsWith("+"))
+        e.patch.split('\n').filter(f => f.startsWith('+'))
       );
       const flatLines = flatten(...actualChanges);
 
       // Only be a dick when they've change enough lined of code.
       if (
         flatLines.length > config.minChangedLines &&
-        flatLines.filter(e => e.startsWith("+//")).length === 0
+        flatLines.filter(e => e.startsWith('+//')).length === 0
       ) {
         const issueComment = context.issue({
           body:
-            "You changed a lot of lines of code but did not add any comments. Shame on you! Shame!!!"
+            'You changed a lot of lines of code but did not add any comments. Shame on you! Shame!!!'
         });
         context.github.issues.createComment(issueComment);
       }
 
       if (config.docscheck) {
-        const statusesToCheck = ["removed", "added"];
+        const statusesToCheck = ['removed', 'added'];
         const docsNeedToBeChecked = allFiles.filter(e =>
           statusesToCheck.includes(e.status)
         );
 
         if (
           docsNeedToBeChecked.length > 0 &&
-          allFiles.filter(e => e.filename.startsWith("docs")).length === 0
+          allFiles.filter(e => e.filename.startsWith('docs')).length === 0
         ) {
           const issueComment = context.issue({
             body:
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
-const nock = require("nock");
+const nock = require('nock');
 // Requiring our app implementation
-const myProbotApp = require("..");
-const { Probot } = require("probot");
+const myProbotApp = require('..');
+const { Probot } = require('probot');
 // Requiring our fixtures
-const payload = require("./fixtures/issues.opened");
-const issueCreatedBody = { body: "Thanks for opening this issue!" };
+const payload = require('./fixtures/issues.opened');
+const issueCreatedBody = { body: 'Thanks for opening this issue!' };
 
 nock.disableNetConnect();
 
-describe("My Probot app", () => {
+describe('My Probot app', () => {
   let probot;
 
   beforeEach(() => {
@@ -17,25 +17,25 @@ describe("My Probot app", () => {
     const app = probot.load(myProbotApp);
 
     // just return a test token
-    app.app = () => "test";
+    app.app = () => 'test';
   });
 
-  test("creates a comment when an issue is opened", async () => {
+  test('creates a comment when an issue is opened', async () => {
     // Test that we correctly return a test token
-    nock("https://api.github.com")
-      .post("/app/installations/2/access_tokens")
-      .reply(200, { token: "test" });
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' });
 
     // Test that a comment is posted
-    nock("https://api.github.com")
-      .post("/repos/hiimbex/testing-things/issues/1/comments", body => {
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/issues/1/comments', body => {
         expect(body).toMatchObject(issueCreatedBody);
         return true;
       })
       .reply(200);
 
     // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
+    await probot.receive({ name: 'issues', payload });
   });
 });
 
